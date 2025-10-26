e3b627013f40c67f458b012ffc30f6581cf62f42
Leandro Stanger <leandrostanger@hotmail.com>
2025-10-26 09:31:00 -0300

    Add files via upload

---
+++ login.js
@@ -0,0 +1,45 @@
+function login() {
+    let emailInput = document.getElementById("loginEmail");
+    let senhaInput = document.getElementById("loginSenha");
+
+    if (!emailInput.checkValidity() || senhaInput.value.trim() === "") {
+        alert("Digite um e-mail válido (com '@' e extensão como '.com', '.br', '.org', etc.) e preencha todos os campos.");
+        return;
+    }
+    
+    const storedEmail = localStorage.getItem("email");
+    const storedSenha = localStorage.getItem("senha");
+    const nomeUsuario = localStorage.getItem("nome");
+    const idadeUsuario = localStorage.getItem("idade");
+
+    if (emailInput.value === storedEmail && senhaInput.value === storedSenha) {
+        alert(`Bem-vindo, ${nomeUsuario}! Você tem ${idadeUsuario} anos.`);
+        window.location.href = "index.html";
+    } else {
+        alert("Credenciais inválidas!");
+    }
+}
+
+function registrar() {
+    let nomeInput = document.getElementById("nome");
+    let idadeInput = document.getElementById("idade");
+    let emailInput = document.getElementById("registrarEmail");
+    let senhaInput = document.getElementById("registrarSenha");
+
+    if (
+        nomeInput.value.trim() === "" ||
+        idadeInput.value.trim() === "" ||
+        !emailInput.checkValidity() ||
+        senhaInput.value.trim() === ""
+    ) {
+        alert("Digite um e-mail válido (com '@' e extensão como '.com', '.br', '.org', etc.) e preencha todos os campos.");
+        return;
+    }
+
+    localStorage.setItem("nome", nomeInput.value.trim());
+    localStorage.setItem("idade", idadeInput.value.trim());
+    localStorage.setItem("email", emailInput.value.trim());
+    localStorage.setItem("senha", senhaInput.value.trim());
+    
+    alert("Usuário registrado com sucesso!");
+}
